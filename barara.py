54a18e7866efe746af40cb2b56df6d52e53a4431
naoshi17 <naoshi.arguello7@gmail.com>
2024-10-09 11:36:58 -0300

    ala

--- barara.py
+++ barara.py
@@ -1,6 +1,13 @@
 import subprocess
 import sys
 
+def install_pip():
+    try:
+        subprocess.check_call([sys.executable, "-m", "ensurepip", "--upgrade"])
+    except subprocess.CalledProcessError:
+        print("Error installing pip")
+        sys.exit(1)
+
 def install_packages():
     packages = [
         "opencv-python==4.6.0.66",
@@ -12,4 +19,8 @@ def install_packages():
         subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
 if __name__ == "__main__":
+    try:
+        subprocess.check_call([sys.executable, "-m", "pip", "--version"])
+    except subprocess.CalledProcessError:
+        install_pip()
     install_packages()
