daefe53aebf50ce51fcbaf8ac4969a9be7e7ad30
naoshi17 <naoshi.arguello7@gmail.com>
2024-10-09 11:42:32 -0300

    ala

--- barara.py
+++ barara.py
@@ -1,12 +1,4 @@
 import subprocess
-import sys
-
-def install_pip():
-    try:
-        subprocess.check_call([sys.executable, "-m", "ensurepip", "--upgrade"])
-    except subprocess.CalledProcessError:
-        print("Error installing pip")
-        sys.exit(1)
 
 def install_packages():
     packages = [
@@ -19,8 +11,4 @@ def install_packages():
         subprocess.check_call(["sudo", "pacman", "-S", "--noconfirm", package])
 
 if __name__ == "__main__":
-    try:
-        subprocess.check_call([sys.executable, "-m", "pip", "--version"])
-    except subprocess.CalledProcessError:
-        install_pip()
     install_packages()
